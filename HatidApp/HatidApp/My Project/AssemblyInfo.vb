05abcfee81cdccd52406ae18d700d0cceec97d3d
Guillido John <424002667@ntc.edu.ph>
2026-04-30 11:49:13 +0800

    #

--- HatidApp/HatidApp/My Project/AssemblyInfo.vb
+++ HatidApp/HatidApp/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("daa84142-1fdf-4162-96b6-433d3373c3d8")>
+<Assembly: Guid("7f46c220-82fe-42c2-b8bc-09ee98cef172")>
 
 ' Version information for an assembly consists of the following four values:
 '
